2f5853cb3d39064ebdf06e08b23b97b5bdf5fa39
budget-pixel <peydevon@gmail.com>
2026-05-26 19:16:55 -0500

    Create walton-budget-search-data.js

---
+++ walton-budget-search-data.js
@@ -0,0 +1,58 @@
+
+
+// walton-budget-search-data.js
+
+window.wcBudgetPages = [
+  {
+    title: "Table of Contents",
+    section: "Introduction and Overview",
+    href: "https://stories.opengov.com/countyofwaltonfl/cf6eaa7a-a98d-479a-9869-b20398ee38e5/published/re0lJHwus?currentPageId=6989dbbdb4696f0b333f2246"
+  },
+  {
+    title: "GFOA Distinguished Budget Presentation Award",
+    section: "Introduction and Overview",
+    href: "https://stories.opengov.com/countyofwaltonfl/cf6eaa7a-a98d-479a-9869-b20398ee38e5/published/re0lJHwus?currentPageId=6989dbbd25815ed4e2fe49b4"
+  },
+  {
+    title: "Transmittal Letter",
+    section: "Introduction and Overview",
+    href: "https://stories.opengov.com/countyofwaltonfl/cf6eaa7a-a98d-479a-9869-b20398ee38e5/published/re0lJHwus?currentPageId=6989dbbdc3a5aa570050fba9"
+  },
+  {
+    title: "Budget-in-Brief",
+    section: "Introduction and Overview",
+    href: "https://stories.opengov.com/countyofwaltonfl/cf6eaa7a-a98d-479a-9869-b20398ee38e5/published/re0lJHwus?currentPageId=69e8ae83a93da8d66dc5e248"
+  },
+  {
+    title: "Overview of Walton County",
+    section: "Introduction and Overview",
+    href: "https://stories.opengov.com/countyofwaltonfl/cf6eaa7a-a98d-479a-9869-b20398ee38e5/published/re0lJHwus?currentPageId=6989dbbd2e29b2249e0a5b99"
+  },
+  {
+    title: "Organizational Structure",
+    section: "Introduction and Overview",
+    href: "https://stories.opengov.com/countyofwaltonfl/cf6eaa7a-a98d-479a-9869-b20398ee38e5/published/re0lJHwus?currentPageId=6989dbbd1af21806371d68e7"
+  },
+  {
+    title: "Strategic Initiatives",
+    section: "Introduction and Overview",
+    href: "https://stories.opengov.com/countyofwaltonfl/cf6eaa7a-a98d-479a-9869-b20398ee38e5/published/re0lJHwus?currentPageId=6989dbbda2feb1e35a5b122d"
+  },
+  {
+    title: "Budget Process & Calendar",
+    section: "Financial Structure, Policies, and Process",
+    href: "https://stories.opengov.com/countyofwaltonfl/cf6eaa7a-a98d-479a-9869-b20398ee38e5/published/re0lJHwus?currentPageId=6989dbbdeb3f9f1b409e86d5"
+  },
+  {
+    title: "Fund Descriptions and Structure",
+    section: "Financial Structure, Policies, and Process",
+    href: "https://stories.opengov.com/countyofwaltonfl/cf6eaa7a-a98d-479a-9869-b20398ee38e5/published/re0lJHwus?currentPageId=6989dbbd623edf6822e6e54d"
+  },
+  {
+    title: "Department to Fund Relationship",
+    section: "Financial Structure, Policies, and Process",
+    href: "https://stories.opengov.com/countyofwaltonfl/cf6eaa7a-a98d-479a-9869-b20398ee38e5/published/re0lJHwus?currentPageId=6989dbbdcf5e99561d6ee920"
+  }
+
+  // Continue pasting the remaining wcBudgetPages objects here.
+];
